2fa597dce2427a6528a241b15e9dd4b47459d4e3
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-22 11:57:35 +0300

    Encapsulate user errors into separate record class

---
+++ Abstractions/UserErrors.cs
@@ -0,0 +1,10 @@
+﻿namespace YTNotifier.Api.Abstractions;
+
+public record UserErrors
+{
+    public static readonly Error InvalidCredentials =
+        new("Auth.InvalidCredentials", "The username or password provided is incorrect.", StatusCodes.Status400BadRequest);
+
+    public static readonly Error DuplicatedEmail =
+        new("User.DuplicatedEmail", "Another user with the same email already exists.", StatusCodes.Status409Conflict);
+}
