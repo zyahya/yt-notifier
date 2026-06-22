fb97010d07a97df0269898b496274502c54cdaef
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-22 11:54:34 +0300

    Update global usings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,10 +2,16 @@ global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 global using System.Text;
 
+global using FluentValidation;
+
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Options;
 global using Microsoft.IdentityModel.Tokens;
 
-global using YTNotifier.Api.Services;
+global using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+
 global using YTNotifier.Api.Entities;
+global using YTNotifier.Api.Services;
