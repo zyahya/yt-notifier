731062bb6893d8dbcdc852d38b9c0bcc0586d5ad
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-22 11:50:59 +0300

    Add jwt options class to provide strongly typed options with validations

---
+++ Services/JwtOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YTNotifier.Api.Services;
+
+public class JwtOptions
+{
+    public const string SectionName = "Jwt";
+
+    [MinLength(30)]
+    public string SecretKey { get; init; } = string.Empty;
+
+    [Required]
+    public string Audience { get; init; } = string.Empty;
+
+    [Required]
+    public string Issuer { get; init; } = string.Empty;
+
+    [Range(300, 3600)]
+    public int ExpiresIn { get; init; }
+}
