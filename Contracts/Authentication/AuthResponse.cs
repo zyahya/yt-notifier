ac98cdcdaccd4b2b7bc97884446d322a9185e9e2
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-22 11:57:57 +0300

    Add required contracts (DTOs)

---
+++ Contracts/Authentication/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace YTNotifier.Api.Contracts.Authentication;
+
+public record AuthResponse(
+    string Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token,
+    int ExpiresIn
+);
---
+++ Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace YTNotifier.Api.Contracts.Authentication;
+
+public record LoginRequest(
+    string Email,
+    string Password
+);
