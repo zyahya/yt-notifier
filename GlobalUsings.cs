1f1501eef7e37e57cc3ab3a66e32931d4a345e10
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-22 11:53:00 +0300

    Update global usings

---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Options;
+global using Microsoft.IdentityModel.Tokens;
+
+global using YTNotifier.Api.Services;
+global using YTNotifier.Api.Entities;
