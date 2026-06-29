ace34caaffb141f68e6e3719222a53e4f53a7b1c
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-06-29 18:57:53 +0300

    Add two more user errors, 'invalid delivery day' and 'invalid delivery hour'

--- Abstractions/UserErrors.cs
+++ Abstractions/UserErrors.cs
@@ -7,4 +7,13 @@ public record UserErrors
 
     public static readonly Error DuplicatedEmail =
         new("User.DuplicatedEmail", "Another user with the same email already exists.", StatusCodes.Status409Conflict);
+
+    public static readonly Error NotFound =
+        new("User.NotFound", "No user found by this identifier.", StatusCodes.Status404NotFound);
+
+    public static readonly Error InvalidDeliveryDay =
+        new("User.InvalidDeliveryDay", "The preferred delivery day must be between 0 (Sunday) and 6 (Saturday).", StatusCodes.Status400BadRequest);
+
+    public static readonly Error InvalidDeliveryHour =
+        new("User.InvalidDeliveryHour", "The preferred delivery hour must be between 0 (12 AM) and 23 (11 PM).", StatusCodes.Status400BadRequest);
 }
