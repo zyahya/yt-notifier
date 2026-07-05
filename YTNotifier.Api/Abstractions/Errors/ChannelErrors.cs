79c697eb5f89272bcb1686aeee762db16300df01
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-07-05 15:41:58 +0300

    Add ChannelErrors class for error handling related to YouTube channels

---
+++ YTNotifier.Api/Abstractions/Errors/ChannelErrors.cs
@@ -0,0 +1,10 @@
+﻿namespace YTNotifier.Api.Abstractions.Errors;
+
+public record ChannelErrors
+{
+    public static readonly Error InvalidChannelId =
+        new("ChannelErrors.InvalidId", "The provided YouTube URL is invalid. Standard channel URLs must include a 24-character ID starting with 'UC'.", StatusCodes.Status400BadRequest);
+
+    public static readonly Error ChannelAlreadyExists =
+        new("ChannelErrors.AlreadyExists", "The provided YouTube channel already exists.", StatusCodes.Status400BadRequest);
+}
