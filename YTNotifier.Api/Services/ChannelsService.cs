20908efcd33fb238a01b2223b8c169a6fad4092c
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-07-09 16:36:33 +0300

    Refactor AddAsync method to use channelUrl and improve subscription handling

--- YTNotifier.Api/Services/ChannelsService.cs
+++ YTNotifier.Api/Services/ChannelsService.cs
@@ -1,4 +1,6 @@
-﻿namespace YTNotifier.Api.Services;
+﻿using System.Text.RegularExpressions;
+
+namespace YTNotifier.Api.Services;
 
 public class ChannelsService : IChannelsService
 {
@@ -11,7 +13,7 @@ public class ChannelsService : IChannelsService
         _context = context;
     }
 
-    public async Task<Result> AddAsync(string userId, string id)
+    public async Task<Result> AddAsync(string userId, string channelUrl)
     {
         var user = await _userManager.FindByIdAsync(userId);
 
@@ -20,13 +22,21 @@ public class ChannelsService : IChannelsService
             return Result.Failure<List<Channel>>(UserErrors.NotFound);
         }
 
-        var channel = new Channel
-        {
-            Id = id
-        };
+        var channelIdResult = GetChannelId(channelUrl);
 
-        user.Channels.Add(channel);
-        await _userManager.UpdateAsync(user);
+        if (channelIdResult.IsFailure)
+        {
+            return Result.Failure(channelIdResult.Error);
+        }
+
+        var channelId = channelIdResult.Value;
+
+        var subscriptionResult = await CreateSubscriptionAsync(user.Id, channelId);
+
+        if (subscriptionResult.IsFailure)
+        {
+            return Result.Failure(subscriptionResult.Error);
+        }
 
         return Result.Success();
     }
@@ -50,5 +60,58 @@ public class ChannelsService : IChannelsService
             .ToListAsync();
 
         return Result.Success(channels);
+    }
+
+    private async Task<Result> SaveUserAsync(ApplicationUser user)
+    {
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded)
+            return Result.Success();
+
+        var error = result.Errors.First();
+
+        return Result.Failure(
+            new Error(error.Code, error.Description, StatusCodes.Status404NotFound));
+    }
+
+    private static Result<string> GetChannelId(string channelUrl)
+    {
+        var match = Regex.Match(channelUrl, YouTubeRegex.ChannelUrl);
+
+        if (!match.Success)
+        {
+            return Result.Failure<string>(ChannelErrors.InvalidChannelId);
+        }
+
+        return Result.Success(match.Groups["id"].Value);
+    }
+
+    private async Task<Result> CreateSubscriptionAsync(string userId, string channelId)
+    {
+        if (!await _context.Channels.AnyAsync(channel => channel.Id == channelId))
+        {
+            await _context.Channels.AddAsync(new Channel
+            {
+                Id = channelId
+            });
+        }
+
+        if (await _context.Subscriptions.AnyAsync(
+            s => s.UserId == userId &&
+            s.ChannelId == channelId))
+        {
+            return Result.Failure(ChannelErrors.AlreadySubscribed);
+        }
+
+        await _context.Subscriptions.AddAsync(new Subscription
+        {
+            UserId = userId,
+            ChannelId = channelId
+        });
+
+        await _context.SaveChangesAsync();
+
+        return Result.Success();
     }
 }
