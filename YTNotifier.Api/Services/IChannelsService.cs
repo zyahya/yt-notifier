089ccc32fa9b2ca33859e21490e5f54deb1d1d7c
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-07-13 15:23:54 +0300

    Refactor IChannelsService interface to use channelUrl parameter for DeleteAsync and AddAsync methods

--- YTNotifier.Api/Services/IChannelsService.cs
+++ YTNotifier.Api/Services/IChannelsService.cs
@@ -1,10 +1,8 @@
-﻿using YTNotifier.Api.Abstractions.Result;
-
-namespace YTNotifier.Api.Services;
+﻿namespace YTNotifier.Api.Services;
 
 public interface IChannelsService
 {
     Task<Result<List<Channel>>> GetAllAsync(string userId);
-    Task<Result> DeleteAsync(string userId, string id);
-    Task<Result> AddAsync(string userId, string id);
+    Task<Result> DeleteAsync(string userId, string channelUrl);
+    Task<Result> AddAsync(string userId, string channelUrl);
 }
