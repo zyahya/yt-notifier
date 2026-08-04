2cc6bf78c33575e9d2e21c18a1a765dbfed1a42e
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-08-04 18:12:26 +0300

    Rename column LastDigestSendAt to NextDigestAt in AspNetUsers table and update model snapshot

---
+++ YTNotifier.Api/Migrations/20260804150509_RenameColumnToNextDigestAt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YTNotifier.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnToNextDigestAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastDigestSendAt",
+                table: "AspNetUsers",
+                newName: "NextDigestAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NextDigestAt",
+                table: "AspNetUsers",
+                newName: "LastDigestSendAt");
+        }
+    }
+}
